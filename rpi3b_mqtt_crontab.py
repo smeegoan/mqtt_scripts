942c8507a3ec9c45aad35902628604e8352630fc
Pedro Cardoso <smeegoan@gmail.com>
2021-07-17 14:04:42 +0100

    fix: run with pi user

--- rpi3b_mqtt_crontab.py
+++ rpi3b_mqtt_crontab.py
@@ -1,6 +1,6 @@
 from crontab import CronTab
 
-cron = CronTab(user='root')
+cron = CronTab(user='pi')
 
 job = cron.new(command='python3 rpi3b_mqtt.py', comment='rpi3b_mqtt')
 job.minute.every(1)
--- rpi4_mqtt_crontab.py
+++ rpi4_mqtt_crontab.py
@@ -1,6 +1,6 @@
 from crontab import CronTab
 
-cron = CronTab(user='root')
+cron = CronTab(user='pi')
 
 job = cron.new(command='python3 rpi4_mqtt.py', comment='rpi4_mqtt')
 job.minute.every(1)
